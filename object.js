a51e0f2b61e9ae180438034c7f1cd25831f3c1b0
mashuk-tamim <tamimmashuk328@gmail.com>
2023-08-02 12:31:57 +0600

    object in JS learned

---
+++ object.js
@@ -0,0 +1,25 @@
+var student={
+    id: 1603113,
+    name: 'Mashuk Tamim',
+    dept: 'CSE',
+    varsity: 'RUET'
+}
+
+var mobile={
+    model: 'Pocophone f1',
+    company: 'Xiaomi',
+    releaseYear: 2018
+}
+
+var computer={
+    model: 'elitebook 840 G3',
+    company: 'HP',
+    processor: 'core i5 gen 6',
+    ram: '8GB'
+}
+//altering the value in a object
+computer.ram='16GB';
+
+console.log(computer.ram);
+console.log(mobile.company);
+console.log(student.id);
